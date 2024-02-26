a688053abd8cf7a34ff5b871baaeea655b6bcad1
redgad88 <vadim_kuzin@list.ru>
2024-02-26 16:01:27 +0300

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
     private int nextId = 1;
     private final Map<Integer, Candidate> candidates = new HashMap<>();
 
@@ -22,10 +21,6 @@ public class MemoryCandidateRepository implements CandidateRepository {
         save(new Candidate(0, "Anton", "Bad bad guy", LocalDateTime.now()));
         save(new Candidate(0, "Valera", "Top notch guy", LocalDateTime.now()));
         save(new Candidate(0, "Dima", "Asshole", LocalDateTime.now()));
-    }
-
-    public static MemoryCandidateRepository getInstance() {
-        return INSTANCE;
     }
 
     @Override
--- src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
@@ -1,7 +1,6 @@
 package ru.job4j.dreamjob.service;
 
 import ru.job4j.dreamjob.model.Vacancy;
-import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
 import ru.job4j.dreamjob.repository.VacancyRepository;
 
 import java.util.Collection;
@@ -9,14 +8,10 @@ import java.util.Optional;
 
 public class SimpleVacancyService implements VacancyService {
 
-    private static final SimpleVacancyService INSTANCE = new SimpleVacancyService();
+    private final VacancyRepository vacancyRepository;
 
-    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
-
-    private SimpleVacancyService() { }
-
-    public static SimpleVacancyService getInstance() {
-        return INSTANCE;
+    public SimpleVacancyService(VacancyRepository vacancyRepository) {
+        this.vacancyRepository = vacancyRepository;
     }
 
     @Override
